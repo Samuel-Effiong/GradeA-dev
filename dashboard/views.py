4721ca34f31486f195e02b32bdd48302996f0bf1
Samuel-Effiong <samueleffiong80@gmail.com>
2025-12-22 15:05:59 +0100

    wip

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,5 +9,6 @@ class DashboardView(TemplateView):
     @action(detail=False, methods=["get"], url_path="stats")
     def overview(self, request, *args, **kwargs):
         # Implementation for stats endpoint
-        # active_course = Course.objects.filter
+        # active_course = Course.objects.filter(is_active=True).count()
+
         pass
