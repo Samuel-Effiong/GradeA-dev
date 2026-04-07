12fabf230ec0b855b382b947494f3154922095ac
Samuel-Effiong <samueleffiong80@gmail.com>
2026-04-07 20:31:59 +0100

    wip

--- assignments/tasks.py
+++ assignments/tasks.py
@@ -316,7 +316,7 @@ def formatted_grade_async(submission_id, user_prompt):
         raise
 
 
-@shared_task(bind=True, max_retries=3)
+@shared_task(bind=True, max_retries=3, soft_time_limit=1800, time_limit=2100)
 def upload_assignment_async(
     self,
     *,
